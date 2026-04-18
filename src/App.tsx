97377730572c9304bdaf1beb278d1b61a86f6457
Huang, Lizhe <e2401837@edu.vamk.fi>
2026-04-18 15:47:50 +0300

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Header from "./components/Header.tsx"
+import Footer from "./components/Footer.tsx"
 
 // 你的页面组件（示例）
 
